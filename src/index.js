8b686d73b5396bd2c9ba7e2652b4452d643b199f
TroyLeung <troylchwork@gmail.com>
2024-10-09 18:30:55 +0800

    fix Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { MobileView, BrowserView } from "react-device-detect";
 import { I18nextProvider } from "react-i18next";
 import "./index.css";
@@ -14,7 +14,7 @@ const customTheme = createTheme({
     primary: "#1975FF",
     darkBeige: "#E6DACE",
     beige: "#F6F4F0",
-    lightBeige: "#BCBEC0",
+    lightBeige: '#BCBEC0',
     boxShadow:
         "0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.09)",
     boxShadowHover:
@@ -24,7 +24,7 @@ const customTheme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Suspense fallback="loading">
-        <BrowserRouter>
+        <HashRouter>
             <I18nextProvider i18n={i18n} defaultNS={"translation"}>
                 <ThemeProvider theme={customTheme}>
                     <AlertProvider>
@@ -32,21 +32,7 @@ root.render(
                     </AlertProvider>
                 </ThemeProvider>
             </I18nextProvider>
-        </BrowserRouter>
-        <MobileView>
-            <div
-                style={{
-                    position: "absolute",
-                    top: 0,
-                    background: "black",
-                    color: "white",
-                    width: "100%",
-                    height: "100%",
-                }}
-            >
-                Sorry, mobile version not publish yet.
-            </div>
-        </MobileView>
+        </HashRouter>
     </Suspense>
 );
 
